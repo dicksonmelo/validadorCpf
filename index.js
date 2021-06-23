866ed0c68e744bd3e8739ce92f08eb8a6d9304c7
digzom <dicksonmelo@gmail.com>
2021-06-23 01:13:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,84 @@
+const cpf = "705.484.450-52";
+let fator = 11;
+
+const limparString = (string, regra = "") => {
+  return string.replace(regra, "");
+};
+
+const limparCpf = (cpf) => {
+  return limparString(cpf, /\D+/g);
+};
+
+const transformarStringEmArray = (string) => {
+  return Array.from(string);
+};
+
+const transformarCpfEmArray = (cpf) => {
+  return transformarStringEmArray(cpf);
+};
+
+const novePrimeirosDigitos = (cpf) => {
+  return transformarCpfEmArray(limparCpf(cpf)).slice(0, -2);
+};
+
+const multiplicarArray = (array, multiplicador) => {
+  return array.map((arrayNum) => arrayNum * (multiplicador -= 1));
+};
+
+const multiplicarCpfArray = (cpfArr, fator) => {
+  return multiplicarArray(cpfArr, fator);
+};
+
+const somarArray = (array) => {
+  return array.reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  );
+};
+
+const retornaNoveCpfSomado = (cpf) => {
+  return somarArray(multiplicarCpfArray(novePrimeirosDigitos(cpf), fator));
+};
+
+const pegarDoisUltimosNumeros = (arr) => {
+  return arr.slice(-2);
+};
+
+const doisUltimosDigitos = (cpf) => {
+  return pegarDoisUltimosNumeros(cpf);
+};
+
+const contaNumerosFinais = (valor) => {
+  const n = 11 - (valor % 11);
+  if (n > 9) return 0;
+  return n;
+};
+
+const dezPrimeirosDigitos = (cpf) => {
+  return transformarCpfEmArray(limparCpf(cpf)).slice(0, -1);
+};
+
+const retornaDezSomado = (cpf) => {
+  return somarArray(multiplicarCpfArray(dezPrimeirosDigitos(cpf), fator + 1));
+};
+
+const getPrimeiroNumeroComProva = (cpf) => {
+  return contaNumerosFinais(retornaNoveCpfSomado(cpf));
+};
+
+const getSegundoNumeroComProva = (cpf) => {
+  return contaNumerosFinais(retornaDezSomado(cpf));
+};
+
+const cpfValidator = (cpf) => {
+  if (
+    getPrimeiroNumeroComProva(cpf).toString() +
+      getSegundoNumeroComProva(cpf).toString() ===
+    doisUltimosDigitos(cpf)
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
+console.log(cpfValidator(cpf));
